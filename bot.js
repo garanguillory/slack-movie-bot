e87c00a83394ea04e9d4a1ac27416f6ea521dc57
Garan Guillory <garanguillory@gmail.com>
2016-07-28 16:03:53 -0500

    Setting up slack movie-bot basics.

---
+++ bot.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var app = express();
+var url = require('url');
+var request = require('request');
+
+var format = ".json";
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port', (process.env.PORT || 9001));
+
+app.get('/', function(req, res){
+  res.send('Running like an 18 wheeler');
+});
+
+app.post('/post', function(req, res){
+  var title = req.body.text;
+
+  var parsed_url = url.format({
+    pathname: 'http://www.omdbapi.com/?t=' + title + format,
+  });
+
+  console.log(parsed_url);
+
+  request(parsed_url, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+      var year = data.Year;
+      var rated = data.Rated;
+      var released = data.Released;
+      var runtime = data.Runtime;
+      var genre = data.Genre;
+      var director = data.Director;
+      var writer = data.Writer;
+
+
+      var body = {
+        response_type: "in_channel",
+        "attachments": [
+          {
+            "text": "Director: " + director + "\n"
+                  + "Rated: " + rated + "\n"
+                  + "Runtime: " + runtime
+          }
+        ]
+      };
+      res.send(body);
+    }
+  });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('App is running on port', app.get('port'));
+});
